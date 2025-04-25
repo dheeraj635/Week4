89ea6eeddcec7d1b4f56c6d9f4d1d65efe2d4ade
dheeraj0810 <dheerajreddy0810@gmail.com>
2025-04-25 12:00:00 +0530

    Java8 Features Problems Solved

---
+++ day6/SquareCalculator.java
@@ -0,0 +1,20 @@
+
+@FunctionalInterface
+interface SquareCalculators {
+    int calculateSquare(int number);
+
+    default void printResult(int number) {
+        int result = calculateSquare(number);
+        System.out.println("The square of " + number + " is: " + result);
+    }
+}
+
+public class SquareCalculator {
+    public static void main(String[] args) {
+       
+        SquareCalculators calculator = num -> num * num;
+
+        calculator.printResult(5);
+        calculator.printResult(12);
+    }
+}
